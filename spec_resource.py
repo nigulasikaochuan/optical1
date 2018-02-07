fb5ceeb439e7b332fa660a15ce2a9a8ec8cb2687
xiaolunzi <nigulasikaochuan@sjtu.edu.cn>
2018-02-07 15:25:05 +0800

    ddd

--- spec_resource.py
+++ spec_resource.py
@@ -18,6 +18,8 @@ class ResourceManageMent(app_manager.RyuApp):
     #resource = ['1999', '2000', '2001', '2002']
 
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    def test(self):
+        pass
     def __init__(self, *args, **kwargs):
         super(ResourceManageMent, self).__init__(*args, **kwargs)
         self.name = 'link_resource'
